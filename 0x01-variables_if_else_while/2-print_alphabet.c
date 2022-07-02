79f677d9b9f18a51545c2e918f3fd9a53018ae8c
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-01 17:10:05 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main- entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+int alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+}
+putchar(alpha);
+}
+return (0);
+}
