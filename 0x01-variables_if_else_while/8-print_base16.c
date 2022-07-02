77b9a9fbe4dcd4048bfbcac4606170f0c53e9b68
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-01 20:07:47 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always
+ */
+int main(void)
+{
+int alpha;
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar(num);
+}
+for (alpha = 'a'; alpha <= 'f'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
