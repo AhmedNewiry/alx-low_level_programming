991216d3d09ff4cee70702ff8d404c4a840f9b12
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-01 20:02:55 -0700

    tebaphla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int alpha;
+for (alpha = 'z'; alpha >= 'a'; alpha--)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
+
