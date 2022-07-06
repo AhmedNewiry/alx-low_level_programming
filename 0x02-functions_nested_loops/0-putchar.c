02f95374652deda732e2f332b6bdfdf9eefbdeef
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-05 17:32:36 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
-*main- entry point
-*Return : Always 0 (sucees)
-**/
+ *main- entry point
+ *
+ *Return: Always 0 (sucees)
+ */
 int main(void)
 {
 _putchar('_');
@@ -15,4 +16,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-} 
+}
