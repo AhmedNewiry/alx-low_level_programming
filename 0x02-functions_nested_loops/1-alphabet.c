60c65d9923417250a367e2f1f81c0534d0eaaf09
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-05 17:35:50 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_alphabet- print all English alphabets
+ */
+void print_alphabet(void)
+{
+int alpha;
+for (alpha = 'a'; alpha <= 'z'; alpah++)
+{
+putchar(alpha);
+}
+putchar('\n');
+}
