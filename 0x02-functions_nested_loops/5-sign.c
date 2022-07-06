d33fd43003e0735dba33a3f7deca54f6148fcb62
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-05 18:34:33 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - checks if the input is a letter:
  * Return: 1 if positve and 0  if zero and -1 if negative
- * @c:is the input 
+ * @c:is the input
  */
 int print_sign(int c)
 {
@@ -13,8 +13,8 @@ return (1);
 }
 else if (c == 0)
 {
-_putchar(0)
-return (0)
+_putchar(0);
+return (0);
 }
 _putchar('-');
 return (-1);
