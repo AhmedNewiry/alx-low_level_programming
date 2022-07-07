44ab62bc040789b0c1c66e3264b2e5fee4097d1f
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-07 05:24:12 -0700

    more nm=umbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * print_most_numbers- prints all numbers except 2 and 4
+ *   */
+void more_numbers(void)
+{
+int x;
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+for (x ='0'; x <= '7*2'; x++)
+{
+_putchar(i);
+}
+_putchar(\n);
+}
+_putchar('\n');
+}
