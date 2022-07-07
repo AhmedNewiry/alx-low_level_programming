8864813b0957ab65482d29abb7d682223061d1c0
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-07 08:32:17 -0700

    pirnt line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include "main"
+
+/**
+ *
+ * *print_line- prints a straight line
+ *
+ * *@n: is length of  the line
+ *
+ * */
+
+void print_line(int n)
+
+{
+
+int i; 
+
+if ( n > 0)
+
+{
+
+for (i = 0; i <= n; i++)
+
+{
+_putchar('_');
+}
+
+}
+
+_putchar('\n');
+
+}
