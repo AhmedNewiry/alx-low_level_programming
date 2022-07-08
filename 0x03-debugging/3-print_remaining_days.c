3b0b923f0e144da82281bdd9ca9cd779cf4a6418
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-08 08:45:21 -0700

    date

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -35,6 +35,7 @@ else
 {
 if (month == 2 && day == 60)
 
+
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
@@ -45,5 +46,4 @@ printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
-
 }
