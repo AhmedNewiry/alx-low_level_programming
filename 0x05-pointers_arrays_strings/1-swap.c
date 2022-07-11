eb7d23895c9173454d24a3fa72c6513dfece4d1d
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-11 10:15:27 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int- tgis function swaps two ibtegers
+ * @a: is tge fist ibteger
+ * @b: is tge second integer
+ */
+
+
+void swap_int(int *a, int *b)
+{
+int *c;
+*c = *a;
+*a = *b;
+*b = *c;
+
+}
