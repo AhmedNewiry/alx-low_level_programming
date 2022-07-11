bd84d1320b74fd0ab232e8492ecf8ddfd5319672
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-11 12:22:44 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts- a function that prints a string
+ * @str: the input pointer of a string
+ */
+void _puts(char *str)
+{
+while (str)
+{
+_putchar(*str);
+str++;
+}
+}
