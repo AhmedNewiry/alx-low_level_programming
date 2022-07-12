0721e10037099e911dcb23b6daa448dedf9e957d
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-12 07:41:49 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _puts- a function that prints a string
+ * puts2- a function that prints a string
  * @str: the input pointer of a string
  */
 void puts2(char *str)
 {
-char *ptr = str;
+
 while (*str)
 {
 
