285bc4fdbda9e96f1f4b3bb9dc43e8d8a60e14d7
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-12 10:20:55 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,6 +8,7 @@ void puts_half(char *str)
 int num1 = 0;
 int num2 = 0;
 int num3 = 0;
+int num4 = 0;
 while (*str)
 {
 num1++;
@@ -21,10 +22,21 @@ str--;
 num2++;
 }
 num3 = num1 / 2;
+num4 = num1 % 2;
+if (!num4)
+{
+while ((num3 - 1) <= num1)
+{
+_putchar(*str);
+num3++;
+str++;
+}
 while (num3 <= num1)
 {
 _putchar(*str);
 num3++;
 str++;
 }
+}
+
 }
