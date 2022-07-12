1b7acc5bf482135e1eb5db93730ecedf1bd42486
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-12 09:31:29 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half- prints half of the string
+ * @str:the pointer to the first character
+ */
+void puts_half(char *str)
+{
+int num1 = 0;
+int num2 = 0;
+int num3 = 0;
+while (*str)
+{
+num1++;
+str++;
+}
+num2 = num1 / 2;
+while (num2 < num1)
+{
+str--;
+num2++;
+}
+num3 = num1/2
+while (num3 < num1)
+{
+_putchar(*str);
+}
+}
