61b1b6722d77a26bd598b21127ac410c18897d02
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-12 16:20:54 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * *_strcpy- copies a string into another one
+ * @dest: is the address of dest is the buffer
+ * @src: is the address of the src
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+dest = src;
+return (dest);
+    
+    
+}
