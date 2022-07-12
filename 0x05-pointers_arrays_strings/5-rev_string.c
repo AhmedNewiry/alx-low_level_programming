56f4530b6c6e6fde0ad2508553e4587343e01fee
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-12 02:58:26 -0700

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,31 @@
+#include "main.h"
+/**
+ * rev_string- reverses aby string
+ * @s: the pointer to the first char of the strin
+ */
 void rev_string(char *s)
 {
 char *start;
 start = s;
+int n = 0;
 while (*s)
 {
 s++;
+n++;
 }
-s--;
+0s--;
+char rev[n];
+int x = 0;
 while (start <= s)
 {
-
+*(&rev + x) = *s;
 --s;
+x++;
 }
-
+int z = 0;
+while (&rev >= start)
+{
+*(s + z) = (&rev + z);
+z++;
+}
 }
