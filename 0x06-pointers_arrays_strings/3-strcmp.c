b915fb4a03d72a73a6476b42878830528460defc
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-15 08:50:03 -0700

    cmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,19 +5,13 @@
  * @s2: the pointer  of the second string
  * Return: 0 if same, >0 if first is greater and <0 if second is greater
  */
+
 int _strcmp(char *s1, char *s2)
 {
-
-int sum1 = 0;
-int sum2 = 0;
 int i = 0;
 while (s1[i] && s2[i])
 {
-
-sum1 += s1[i];
-sum2 += s2[i];
-
-if (sum1 != sum2)
+if (s1[i] != s2[i])
 {
 return (s1[i]  - s2[i]);
 }
