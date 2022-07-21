2dcb398c8d5538d57e788c1833073ea097fededf
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-21 15:16:14 -0700

    str

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strspn- Returns the number of bytes of s that equal the bytes from accept
+ * @s: pointer to the first sting
+ * @accept: pointer to the second string
+ * Retrun: number of bytes if found null if not
+ */
+
+
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+unsigned int i;
+unsigned int x;
+unsigned int r = 0;
+
+for (i = 0; s[i]; i++)
+{
+if (s[i] != 32)
+{
+for (x = 0; accept[x]; x++)
+{
+if (s[i] == accept[x])
+{
+r++;
+}
+
+
+}
+
+}
+else
+{
+return (r);
+}
+}
+return (r);
+}
