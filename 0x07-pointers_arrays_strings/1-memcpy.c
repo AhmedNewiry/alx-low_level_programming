3089aa93d8c3a6d0ef461fbe521bcfe6ffee6c55
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-21 09:30:53 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,6 +4,7 @@
  * @dest: the pointer to the first element of dest array
  * @src: the pointer to the first element of src array
  * @n: the number of elements to be copied
+ * Return: pointer to dest array
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
