01748605d42e77e93ac5ec947bbd1c2c21f6a73b
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-24 06:28:32 -0700

    _puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * _puts_recursion- a function that prints a string
+ * @s: the pointer to the first character of the string
+ * Return: 0 if success
+ */
+
+
+void _puts_recursion(char *s)
+{
+if (!*s)
+{
+return (0);
+}
+_putchar(*s);
+_puts_recursion((*s + 1));
+return (0);
+
+}
