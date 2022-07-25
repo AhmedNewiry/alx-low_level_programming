557e07c933a5f9f1bbb4e723e9526f0d04faefde
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-25 10:40:05 -0700

    copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * _strdup- copy of the string given as a parameter.
+ * @str: a pointer to the first char of the stirng
+ * Return: pointer to the first element if success and NULL if not
+ */
+
+
+char *_strdup(char *str)
+{
+int x;
+int i;
+char *new = NULL;
+for (x = 0; str[x] != '\0', i++)
+{
+x++;
+}
+if (str)
+{
+new = malloc(x * sizeof(char));
+for (i = 0; str[i] != '\0'; i++)
+{
+str[i] = new[i];
+}
+new[i++] = '\0';
+return (new);
+}
+return (NULL);
+}
