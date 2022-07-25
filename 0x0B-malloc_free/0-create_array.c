fae67e5d38afd8af7686b4223a3c8ffc66eeaa42
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-25 09:24:18 -0700

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,13 +11,17 @@
 
 char *create_array(unsigned int size, char c)
 {
+unsigned int i;
 char *ar;
 ar = (char *)malloc(size * sizeof(char));
 if (ar == NULL)
 {
 return (NULL);
 }
-ar[0] = c;
+for (i =0; i < size; i++)
+{
+ar[i] = c;
+}
 return (ar);
 
 }
