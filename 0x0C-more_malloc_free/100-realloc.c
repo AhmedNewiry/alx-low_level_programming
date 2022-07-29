35814251c8fbb03e46fc5c5b499570f8afd22ed4
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-29 14:24:55 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,40 +9,32 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-unsigned int x = 0;
+
 unsigned int i = 0;
-char *new_ptr = ptr;
 char *new_array = NULL;
 if (new_size == old_size)
 {
 return (ptr);
 }
-if (new_size == 0)
+if (new_size == 0 && ptr)
 {
 free(ptr);
-free(new_ptr);
-return (NULL);
-}
-while (new_ptr[x])
-{
-x++;
-}
-if (!ptr)
-{
-new_array = malloc(new_size);
-return (new_array);
-}
-new_array = malloc(sizeof(*new_array) * new_size);
-if (new_array)
-{
-while (i < x)
-{
-new_array[i] = new_ptr[i];
-}
-free(ptr);
-free(new_ptr);
-return (new_array);
-}
+
 return (NULL);
 }
 
+
+new_array = malloc(new_size);
+if (new_array)
+{
+while (i < new_size)
+{
+new_array[i] = 0;
+i++;
+}
+free(ptr);
+return (new_array);
+}
+
+return (NULL);
+}
