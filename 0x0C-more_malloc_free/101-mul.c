795406e12969ff254cab8e9f6cc366032f3d2863
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-30 03:25:27 -0700

    mulyi :

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,13 +4,14 @@
  * main- entry point
  * @argc: number of command line arguments
  * @argv: command line arguments
+ * Return: 0 if sucess 
  */
 int main(int argc, char **argv)
 {
 if (argc == 3)
 {
 printf("%i", atoi(argv[1]) * atoi(argv[2]));
-return(0);
+return (0);
 }
 printf("Error");
 exit(98);
