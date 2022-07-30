062b9ec7a3f29f3cd1119e7a7a97cc2cb058557d
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-07-30 03:29:14 -0700

    mult

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,10 +10,10 @@ int main(int argc, char **argv)
 {
 if (argc == 3)
 {
-printf("%i", atoi(argv[1]) * atoi(argv[2]));
+printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
-printf("Error");
+printf("Error\n");
 exit(98);
 
 }
