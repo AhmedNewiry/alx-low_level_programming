0283807ec738f723dd84d4e6eaa35a11dda4b376
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-01 16:31:57 -0700

    print

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,12 +4,13 @@
  * main- the main entry point
  * @argc: the number of command line arguments
  * @argv: the command line arguments
+ * Return: Always 0
  */
 int main(int argc, char **argv)
 {
 if (argv)
 {
-printf("%s", argv[0]);    
+printf("%s", argv[0]);
 }
 return (0);
 }
