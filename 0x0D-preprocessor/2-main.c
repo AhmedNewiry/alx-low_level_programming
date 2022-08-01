e75862b0db96d0f3e401e40e0b03595d8579eea8
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-01 16:30:02 -0700

    print

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- the main entry point
+ * @argc: the number of command line arguments
+ * @argv: the command line arguments
+ */
+int main(int argc, char **argv)
+{
+if (argv)
+{
+printf("%s", argv[0]);    
+}
+return (0);
+}
