7648d7368c1efc708dd7e82b37fa71a9aa8bc8cc
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-01 15:05:34 -0700

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * free_dog- Write a function that frees dogs.
+ * @d: the pointer to dog structure
+ */
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);    
+}
+
+}
+
