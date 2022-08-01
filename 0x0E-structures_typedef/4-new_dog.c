6fd5c1effdb48ce650fdd19e8b4866f1b4bbc25f
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-01 14:00:43 -0700

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * new_dog- a function that creates a new dog.
+ * @name: first paramater
+ * @age:second paramater
+ * @owner: third paramater
+ * Return: the address to the new struct if suceess or NULL if not
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *new_dog;
+new_dog = malloc(sizeof(new_dog));
+if (new_dog)
+{
+new_dog->name = name;
+new_dog->age = age;
+new_dog->owner = owner;
+return (new_dog);
+}
+return (new_dog);
+}
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,7 @@ float age;
 };
 
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 int _putchar(int c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
