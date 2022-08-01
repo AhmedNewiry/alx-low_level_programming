a136cebcf5aaacb538f7f49616d79dfeb736cc1e
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-01 13:21:59 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,7 @@
 void print_dog(struct dog *d)
 {
 d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
-isdigit(d->age) ? (printf("Age: %f\n", d->age)) : (printf("Age: (nil)\n"));
+printf("Age: %f\n", d->age);
 d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
 
 }
