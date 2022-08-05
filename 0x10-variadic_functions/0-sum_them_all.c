bb84f4165ff8e264f945ef831aafac72ff9ff386
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-05 15:20:01 -0700

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,12 +9,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i;
 unsigned int sum = 0;
+va_list ptr;
 if (n == 0)
 {
 return (0);
 }
 
-va_list ptr;
 va_start(ptr, n);
 for (i = 0; i < n; i++)
 {
