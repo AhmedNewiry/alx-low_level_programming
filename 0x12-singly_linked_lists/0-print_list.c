52ce7b66b1654258bacf07e346850a2860f6d502
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-12 09:31:21 -0700

    print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * Print_list-a function that prints all the elements of a list_t list.
+ * print_list-a function that prints all the elements of a list_t list.
  * @h: pointer to the first element in the list
  * Return: the size of the linked list
  */
