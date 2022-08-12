5a63cfb59f5d438ed54e1f10ad045ca02aa246c7
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-12 11:52:59 -0700

    add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -24,11 +24,12 @@ return (i);
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *temp;
+list_t *new_node;
 if (str == NULL)
 {
 return (NULL);
 }
-list_t *new_node = malloc(sizeof(list_t));
+new_node = malloc(sizeof(list_t));
 if (new_node != NULL)
 {
 new_node->str = strdup(str);
