6625040fd8d84135dfddf261fae159e1080b1fe8
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-13 03:54:50 -0700

    add end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -24,35 +24,38 @@ return (i);
 list_t *add_node_end(list_t **head, const char *str)
 {
 
-list_t *new_node;
-list_t *temp;
+list_t *new_node = NULL;
+list_t *temp = NULL;
 
 if (str == NULL)
 {
 return (NULL);
 }
 new_node = malloc(sizeof(list_t));
+
 if (new_node != NULL)
 {
 new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
 new_node->len = _strlen(new_node->str);
-new_node->next = NULL;
 
+new_node->next = NULL;
 if (*head == NULL)
 {
 *head = new_node;
 return (new_node);
 }
 temp = *head;
-while (temp != NULL)
+while (temp->next)
 {
 temp = temp->next;
-
 }
-
-temp = new_node;
+temp->next = new_node;
 return (new_node);
 }
-
 return (NULL);
 }
