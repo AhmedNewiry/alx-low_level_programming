7f5863c0279cce00e8872113e5be202344a5cc84
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-17 03:56:00 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,44 @@
 #include "lists.h"
+
+/**
+ *
+ * listint_len- a function that returns the size of a linked
+ *
+ * @h : pointer to the first element of the list
+ *
+ * Return: the size of the linked list
+ *
+ */
+
+size_t listint_len(const listint_t *h)
+
+{
+
+	size_t i = 0;
+
+	if (h == NULL)
+
+	{
+
+		return (i);
+
+	}
+
+	while (h != NULL)
+
+	{
+
+		h = h->next;
+
+		i++;
+
+	}
+
+	return (i);
+
+
+
+}
 /**
  * insert_nodeint_at_index- a function that inserts a node at a given index.
  * @head:double pointer to the first element in the list
