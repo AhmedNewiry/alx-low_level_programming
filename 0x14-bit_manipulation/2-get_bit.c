d24a9609cda84124d0d5a551e5d41264b484f7fb
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-19 14:38:33 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,26 +7,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int number;
-	unsigned int x;
 	unsigned int bitNum;
 	unsigned int binary;
 
-	number = n;
-
-	for (x = 0; n != 0; x++)
-	{
-		n = n >> 1;
-	}
-	if (index > x - 1)
-	{
-		return (-1);
-	}
 	bitNum = ((sizeof(unsigned long int) * 8) - 1);
 	if (index > bitNum)
 	{
 		return (-1);
 	}
-	binary = ((number >> index) & 1);
+	binary = ((n >> index) & 1);
 	return (binary);
 }
