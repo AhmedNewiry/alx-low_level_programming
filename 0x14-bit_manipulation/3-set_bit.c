a6c7cdce063d24722f5a3833a508531961f6264c
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-19 15:56:55 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -17,7 +17,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	{
 		return (-1);
 	}
-	binary = ((EditedNum >> index) | 1);
+	binary = ((EditedNum >> index) & 1);
 	*n = (*n | 1 << index);
 
 
