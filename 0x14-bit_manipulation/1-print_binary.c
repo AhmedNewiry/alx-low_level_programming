150b2c382620dc1b79a9f262ddfe857cf86a07d1
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-18 18:39:28 -0700

    binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,26 +5,25 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int number;
-	int x;
-	number= n;
+unsigned long int number;
+int x;
+number = n;
 
-	if (n == 0)
-	{
-				putchar('0');
-						return;
-	}
+if (n == 0)
+{
+_putchar('0');
+return;
+}
 
-	for (x = 0; n != 0; x++)
-	{
-			    n = n >> 1;
-	}
+for (x = 0; n != 0; x++)
+{
+n = n >> 1;
+}
 
-	for (x -= 1; x >= 0; x--)
-	{
+for (x -= 1; x >= 0; x--)
+{
+_putchar('0' + ((number >> x) & 1));
 
-		putchar('0' + ((number >> x) & 1));
-
-	}
+}
 
 }
