e231bf387704bb397f1ccc0da954dfbf0a3de9fa
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-19 18:58:17 -0700

    flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * flip_bits-function that returns the number\
+ of different bits between 2 nums
+ * @n: the first number
+ * @m: the second number
+ * Return: the number of different bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff;
+	int diffCount = 0;
+
+	diff = n ^ m;
+	while (diff != 0)
+	{
+		if (diff & 1 == 1)
+		{
+			diffCount++;
+		}
+		diff = diff >> 1;
+	}
+	return (diffCount);
+}
