7493fa35bf1ff8e4f2fb72373c90292e8e02948d
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-21 06:29:43 -0700

    single

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 /**
  * main - entry point
- *
  * Return: Always 0
  */
 int main(void)
 {
-int fir;
-int sec;
-for (fir = '0'; fir <= '9'; fir++)
-{
-putchar(fir);
-for (sec = '0'; sec <= '9'; sec++)
-{
-putchar(',');
-putchar(sec);
+	int fir;
+	int sec;
+
+	for (fir = '0'; fir <= '9'; fir++)
+	{
+		putchar(fir);
+		if (fir < '9')
+		{
+			putchar(',');
+			putchar(sec);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-}
-putchar('\n');
-return (0);
-}
+
