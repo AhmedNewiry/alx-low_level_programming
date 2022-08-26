3fd933db2f3b06d8d5581fa3f4039a9f6703277f
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-26 02:28:34 -0700

    cearte

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen- a function that finds the length of a string
+ * @str: a pointer to the string
+ * Return: the length of a string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int  x = 0;
+
+	while (str[x])
+	{
+		x++;
+	}
+	return (x);
+}
+
+
+
+/**
+ * create_file-a function that creates a file.
+ * @filename: a pointer to the filename
+ * @text_content: pointer to the text to be written in the file
+ * Return: 1 if success or -1 if fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fildes;
+	int len;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fildes = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fildes == -1)
+	{
+		return (-1);
+	}
+	len = _strlen(text_content);
+	if (text_content != NULL)
+	{
+		write(fildes, text_content, len);
+		close(fildes);
+		return (1);
+	}
+
+	return (-1);
+
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(int c);
+int create_file(const char *filename, char *text_content);
+unsigned int _strlen(char *str);
 #endif
 
