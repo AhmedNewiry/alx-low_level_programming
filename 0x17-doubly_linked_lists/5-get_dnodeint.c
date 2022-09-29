70a9834a406594393cae9973922f9965c8d7626a
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-29 14:17:09 -0700

    add

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,11 +10,12 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *temp;
+
 	size_t i = 0;
 	temp = head;
 	if (index == 0)
 	{
-		return(head);
+		return (head);
 	}
 	while (temp->next)
 	{
