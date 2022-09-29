13d64eac360a94dab740e849e1590117cd431773
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-09-29 05:39:49 -0700

    print

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print-dlistint- a function that prints all data of a linked list
+ * print_dlistint- a function that prints all data of a linked list
  * @h: pointer to the linked list head
  * Return: the length of the linked list
  */
