8460c059b2f473c9e4fbe2a243f62c1b684965df
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-11-02 03:09:23 -0700

    set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node->value = strdup(value);
 	new_node->key = key;
-	index = key_index(key,ht->size);
+	index = key_index((const unsigned char *)key,ht->size);
 	if (ht[index] == NULL)
 	{
 		ht[index] = new_node;
