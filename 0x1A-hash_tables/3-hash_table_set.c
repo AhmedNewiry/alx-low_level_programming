b3d7a12176934de435d7677aef48ff761924e9ca
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-11-02 02:28:23 -0700

    populate

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set-a function that adds an element to the hash table.
+ * @ht: the hast table
+ * @key: the key to generate index
+ * @value: the value to be inserted into a specific index
+ * Return: 1 if success or 0 if not
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+	hash_node_t *temp;
+
+
+	if (!key)
+	{
+		return (0);
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+	{
+		return (0);
+	}
+	new_node->value = strdub(value);
+	new_node->key = key;
+	index = key_index(key);
+	if (ht[index] == NULL)
+	{
+		ht[index] = new_node;
+		new_node->next = NULL;
+		return (1);
+	}
+
+
+	temp = new_node;
+	new_node->next = ht[index];
+	ht[index] = temp;
+	return (1);
+
+}
