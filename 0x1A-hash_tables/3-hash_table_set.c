e1310e05e9c34005091a9f595a6837a6348cb45a
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-11-02 02:30:48 -0700

    populate

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		return (0);
 	}
-	new_node->value = strdub(value);
+	new_node->value = strdup(value);
 	new_node->key = key;
 	index = key_index(key);
 	if (ht[index] == NULL)
